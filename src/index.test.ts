5fed8656f7411ba10b49e5af6d904500d74bd80f
Christian Alfoni <christianalfoni@gmail.com>
2023-04-06 10:52:53 +0000

    Update index.test.ts

--- src/index.test.ts
+++ src/index.test.ts
@@ -44,29 +44,6 @@ describe("States", () => {
     await transition;
     expect(states.get()).toEqual({ status: "CONNECTED", ref: 0 });
   });
-  it("should ignore same transition", async () => {
-    const states = new States<TestState, TestTransitionState>({
-      status: "DISCONNECTED",
-    });
-
-    const transition = states.transition(
-      { status: "CONNECTING" },
-      async () => ({ status: "CONNECTED", ref: 0 })
-    );
-
-    expect(states.get()).toEqual({ status: "CONNECTING" });
-    const secondTransition = states.transition(
-      { status: "CONNECTING" },
-      async () => ({
-        status: "CONNECTED",
-        ref: 1,
-      })
-    );
-    const resultA = await transition;
-    expect(resultA).toEqual({ status: "CONNECTED", ref: 0 });
-    const resultB = await secondTransition;
-    expect(resultB).toEqual({ status: "CONNECTED", ref: 0 });
-  });
   it("should wait for current transitions", async () => {
     const states = new States<TestState, TestTransitionState>({
       status: "DISCONNECTED",
@@ -77,7 +54,7 @@ describe("States", () => {
       ref: 0,
     }));
 
-    const state = await states.whenTransitioned();
+    const state = await states.resolve();
 
     expect(state).toEqual({ status: "CONNECTED", ref: 0 });
   });
